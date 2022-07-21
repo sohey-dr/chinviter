14ef093bc5a8264d5ef82c15ef8f59567171f891
soheyHashimoto <heyso1209@gmail.com>
2022-07-21 21:19:48 +0900

    feat: call get_request_slack_api and check reponse

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-#![allow(unused)]
-
+use reqwest;
 use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    pattern: String,
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
+    token: String,
 }
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    let resp = get_request_slack_api("conversations.list", &args.token);
+    match resp {
+        Ok(resp) => {
+            println!("{:?}", resp.text());
+        }
+        Err(e) => {
+            println!("Error: {}", e);
         }
     }
 }
